70b153ea05045bc20697404916a064b9cf1e32c3
Matthias Pigulla <mp@webfactory.de>
2022-05-25 14:55:16 +0000

    Avoid using PHPUnit-speficic exception classes

--- src/Test/ShortcodeTest.php
+++ src/Test/ShortcodeTest.php
@@ -26,7 +26,7 @@ abstract class ShortcodeTest extends WebTestCase
         parent::setUp();
 
         if ('' === $this->getShortcodeToTest() || null === $this->getShortcodeToTest()) {
-            throw new PHPUnit_Framework_IncompleteTestError('Albeit being a '.__CLASS__.', '.static::class.' does not define a shortcode to test.');
+            throw new \RuntimeException('Albeit being a '.__CLASS__.', '.static::class.' does not define a shortcode to test.');
         }
 
         static::bootKernel();
@@ -55,7 +55,7 @@ abstract class ShortcodeTest extends WebTestCase
 
         $crawlerOnRenderedExample = $crawlerOnRenderedExamplePage->filter('#rendered-example');
         if (0 === $crawlerOnRenderedExample->count()) {
-            throw new PHPUnit_Framework_ExpectationFailedException('No rendered example found for shortcode "'.$this->shortcode.'"');
+            throw new \RuntimeException('No rendered example found for shortcode "'.$this->shortcode.'"');
         }
 
         return $crawlerOnRenderedExample;
