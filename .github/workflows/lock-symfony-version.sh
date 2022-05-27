be5f242ca79019f897272d3e39ccb410f9187782
Matthias Pigulla <mp@webfactory.de>
2022-05-27 15:12:04 +0000

    Update test matrix to cover multiple PHP versions

--- .github/workflows/lock-symfony-version.sh
+++ .github/workflows/lock-symfony-version.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-sed --in-place --regexp-extended --expression='s/"(symfony\/.*)": "\^.*"/"\1": "'$VERSION'"/' composer.json
+cat <<< $(jq --arg version $VERSION '.require  |= with_entries(if ((.key|test("^symfony/service-contracts")|not) and (.key|test("^symfony/"))) then .value=$version else . end)' < composer.json) > composer.json
