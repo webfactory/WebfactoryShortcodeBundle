246eff9da33bffeb9e68634fe7a968bde8f0c5d7
Matthias Pigulla <mp@webfactory.de>
2024-09-04 12:27:04 +0200

    Nutze den DebugClassLoader, um fehlende Return Types aufzuspüren

--- src/DependencyInjection/Compiler/ShortcodeCompilerPass.php
+++ src/DependencyInjection/Compiler/ShortcodeCompilerPass.php
@@ -13,7 +13,7 @@ use Webfactory\ShortcodeBundle\Controller\GuideController;
  */
 class ShortcodeCompilerPass implements CompilerPassInterface
 {
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         $shortcodeServices = $container->findTaggedServiceIds('webfactory.shortcode');
 
