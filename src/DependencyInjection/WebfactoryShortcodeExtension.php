d224d1eeab34806c3f12bd2bc972be8b4126f010
Martin Güthler <mg@webfactory.de>
2025-07-31 15:43:15 +0200

    Vermeide Deprecation
    ...durch Nutzung von `Symfony\Component\DependencyInjection\Extension\Extension` an Stelle von `Symfony\Component\HttpKernel\DependencyInjection\Extension`.

--- src/DependencyInjection/WebfactoryShortcodeExtension.php
+++ src/DependencyInjection/WebfactoryShortcodeExtension.php
@@ -5,8 +5,8 @@ namespace Webfactory\ShortcodeBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Loads the bundle configuration.
